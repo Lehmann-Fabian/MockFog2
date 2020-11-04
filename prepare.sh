e626f5be3c00debd3e75d340972e274aabd825f3
Lehmann-Fabian <fabiampfuhl@gmx.de>
2020-11-04 12:56:35 +0100

    timeout for destroy

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 cd node-manager
-node app.js agent
-node app.js manipulate
+
+code=124
+
+while [[ "$code" -eq 124 ]]; do
+
+timeout --foreground 7m bash <<EOT
+    node app.js agent
+    node app.js manipulate
+EOT
+code=$?
+
+done
+
+
 cd ..
